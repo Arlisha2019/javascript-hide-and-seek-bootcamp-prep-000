11e207bd9031cbe7a91c8db411538b022dd33792
Arlisha2019 <arlisha_hayles@yahoo.com>
2018-08-20 22:34:51 +0000

    Done.

--- index.js
+++ index.js
@@ -2,3 +2,24 @@ function getFirstSelector(selector) {
  return document.querySelector(selector);
 }
 
+function nestedTarget() {
+ return document.querySelector('div.target');
+}
+
+function deepestChild() {
+  var current = document.getElementById('grand-node');
+  var nextNode = current.children[0];
+  while (nextNode) {
+   current = nextNode;
+   nextNode = current.children[0];
+  }
+ return current 
+}
+
+function increaseRankedBy(n) {
+  var newVar = document.getElementById('app').querySelectorAll('ul.ranked-list li');
+  var increase = parseInt(n);
+  for (var i = 0; i < increase.length; i++); {
+  }
+  return 
+}
